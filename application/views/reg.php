eb90dc8b06d0eccc60e735cb7262bdec9230845d
Betta <21828604@qq.com>
2015-09-10 10:34:52 +0800

    Update reg.php

--- application/views/reg.php
+++ application/views/reg.php
@@ -11,35 +11,24 @@ window.onload=function(){
 $name=document.getElementById("name_1");
 $department=document.getElementById("department_1");
 $phone=document.getElementById("phone_1");
-$photo=document.getElementById("file_1");
-
 $departmentre=document.getElementById("departmentre");
 $phonere=document.getElementById("phonere");
 $namere=document.getElementById("namere");
-
-$NameReg=0;
-$PhoneReg=0;
-$DepartmentReg=0;
-
 $department.onkeyup=function(){//验证学院//
 
 	if($department.value.match('^[\u2E80-\u9FFF]{2,7}$')==null){
 		$departmentre.style.display="table-row";
-		$DepartmentReg=0;
 	}
 
-	else {$departmentre.style.display="none";
-	$DepartmentReg=1;}
+	else $departmentre.style.display="none";
 }
 
 $phone.onkeyup=function(){//验证电话//
 
 	if ($phone.value.match('^1[0-9]{10}$')==null){
 		$phonere.style.display="table-row";
-		$PhoneReg=0;
 	}
-	else {$phonere.style.display="none";
-		  $PhoneReg=1;}
+	else $phonere.style.display="none";
 
 }
 
@@ -48,45 +37,23 @@ $name.onkeyup=function(){//验证姓名//
 
 	if ($name.value.match('^[\u2E80-\u9FFF]{2,4}$')==null){
 		$namere.style.display="table-row";
-		$NameReg=0;
 	}
-	else {$namere.style.display="none";
-	$NameReg=1;}
+	else $namere.style.display="none";
+	}
 }
+</script>
 
-}
-
-function valid()
-{
-	if($NameReg==0)
-	{
-	alert("请填写二到四个字的姓名");
-	return false;
-	}
-	else if($DepartmentReg==0)
-	{
-	alert("请填写正确的学院名");
-	return false;
-	}
-	else if($PhoneReg==0)
-	{
-	alert("请填写13位手机号");
-	return false;
-	}
-	else if ($photo.value=="")
-	{
-	alert("请上传照片");
-	return false;
-	}
-	else if ($photo.value.split(".")[1]!="gif"||$photo.value.split(".")[1]!="png"||$photo.value.split(".")[1]!="jpg")
-	{
-	alert("请上传gif，png，jpg格式的文件");
-	return false;
-	}
-
-	else return true;
-}
-
+<script type="text/javascript">
+$(function(){
+    $('#img_upload').change(function(){
+      var file = this.files[0]; //选择上传的文件
+      var r = new FileReader();
+      r.readAsDataURL(file); //Base64
+        $(r).load(function(){
+          $('#box').html('<img style="width:154px; height:217px;" src="'+ this.result +'" alt="" />');
+        });
+    });
+});
 </script>
 
 <title>Hgdonline报名</title>
@@ -96,7 +63,7 @@ function valid()
 <div class="main container" >
   <div class="content" id="content">
     <form action="reg/post" method="post" enctype="multipart/form-data">
-	  <table   class="table">
+	  <table  class="table">
 	    <tr >
 		  <td>
 			<input id="name_1" class="input_1" type="text" name="username" placeholder="姓名"  />
@@ -108,7 +75,8 @@ function valid()
 		  <td  rowspan="3">
 			<div  class="upload">
 				<input type="text" class="upload_1" name="txt" value="上传照片"/>
-				<input id="file_1" type="file" class="upload_2" name="uploadFile"/>
+				<input type="file" id="img_upload" name="userfile"/>
+				<div id="box" style="position: absolute; margin: -219px 0 0 0;"></div>
 			</div>
 		  </td><!--picture voer-->
 		</tr>
@@ -128,16 +96,16 @@ function valid()
 		</tr>
 
 		<tr>
-		 <td id="departmentre"  class="rege2"> 请输入二到四个字的姓名
+		 <td id="departmentre"  class="rege2"> 请输入二到七个字的年级专业
 		 </td>
 		</tr>
 
 
 		<tr >
 		  <td class="range3">
-		      <label><input class="radio_1" type="radio" name="intention" value= "技术部" checked="checked" />    技术部</label>
-		      <label><input class="radio_1" type="radio" name="intention" value="产品部"  />    产品部</label>
-		      <label><input class="radio_1" type="radio" name="intention" value="产品部"  />    设计部</label>
+		      <label><input class="radio_1" type="radio" name="Intention" value= "技术部" checked="checked" />    技术部</label>
+		      <label><input class="radio_1" type="radio" name="Intention" value="产品部"  />    产品部</label>
+		      <label><input class="radio_1" type="radio" name="Intention" value="产品部"  />    设计部</label>
 		  </td><!--intention voer-->
 		  <td class="range4">
 			<input id="phone_1" class="input_1" type="text" name="phone" placeholder="手机号" />
@@ -145,7 +113,7 @@ function valid()
 		</tr>
 
 		<tr>
-		 <td id="phonere" class="rege3"> 请输入二到四个字的姓名</td>
+		 <td id="phonere" class="rege3"> 请输入十一位手机号</td>
 		</tr>
 
 	  </table><!--table voer-->
@@ -153,7 +121,7 @@ function valid()
 	    <textarea class="input_3" id="summary" placeholder="自我介绍" name="summary" ></textarea>
 	  </div><!--textarea voer-->
 	  <div >
-		<button id="buttons"  class="submit_1" type="submit"  name="submit_1" onclick="valid();" >提     交</button>
+		<button class="submit_1" type="submit"  name="submit"/>提     交</button>
 	  </div><!--submit voer-->
     </form>
   </div>
